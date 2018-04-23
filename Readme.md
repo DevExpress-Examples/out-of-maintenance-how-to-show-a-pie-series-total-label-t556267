4ba85f99e9ba1da49147d572ada03f6bad5eb5f0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: show a Pie series' total label
+
+
+This example demonstrates how to show a Pie series' total label.
+
+
+<h3>Description</h3>
+
+Use the <strong>PieSeriesView.TotalLabel</strong> property to access settings that configure a Pie (Doughnut) series' total label.
+
+<br/>
+
+
---
+++ VB/PieTotalLabelSample/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports DevExpress.XtraCharts
+Imports System
+Imports System.Windows.Forms
+
+Namespace PieTotalLabelSample
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+'            #Region "#PieTotalLabel"
+            Dim totalLabel As PieTotalLabel = CType(pieChart.Series("Country Areas").View, DoughnutSeriesView).TotalLabel
+            totalLabel.Visible = True
+            totalLabel.TextPattern = "Total area" & vbLf & "{TV:F2} km" & ChrW(&HB2).ToString()
+'            #End Region ' #PieTotalLabel
+        End Sub
+    End Class
+End Namespace
